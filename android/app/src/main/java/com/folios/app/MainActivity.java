d6c50ed75237b48ba825c8b0629590c9074c31db
DaiichiroNoda <69710300+donachii@users.noreply.github.com>
2022-11-08 00:56:59 +0900

    add ToDo task

--- android/app/src/main/java/com/folios/app/MainActivity.java
+++ android/app/src/main/java/com/folios/app/MainActivity.java
@@ -5,11 +5,5 @@ import android.os.Bundle;
 import com.folios.plugin.w3acustom.W3aCustomPlugin;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            registerPlugin(W3aCustomPlugin.class);
-            super.onCreate(savedInstanceState);
-        }
-}
+public class MainActivity extends BridgeActivity {}
 
